b5f6c37906eaf43aa32fbd70cf37a577a37abc0b
Shaikh Aisha Mohd Aslam <aishaaslam2016@gmail.com>
2023-05-18 15:47:38 +0530

    first
    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HeroService } from './hero.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
 }
